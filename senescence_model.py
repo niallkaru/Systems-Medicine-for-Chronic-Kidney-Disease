6ab4017e2a8221d643f0da9cbff2ca4ced75d4aa
niallkaru <niallkarunaratne@gmail.com>
2024-12-21 18:47:15 +0000

    Decided to take a class-based approach for the project, updating the senescence model to reflect this also added a main.py file, as yet unused.

--- senescence_model.py
+++ senescence_model.py
@@ -1,18 +1,34 @@
+### Class for senescence model for cells (within kidney), created by Niall
+### Karunaratne 21/12/2024
 import numpy as np
-import matplotlib.pyplot as plt
 from scipy.integrate import solve_ivp,odeint
-
-def senescent_model(t,x,params):
-    eta = params[0]
-    beta = params[1]
-    kappa = params[2]
-
-    dxdt = eta*x-(beta*x)/(x-kappa)
-    return dxdt
-t = np.linspace(0,365,365)
-sol = solve_ivp(senescent_model, [t[0],t[-1]],[5],args=([0.084,0.15,0.5],))
-print(len(sol.t))
-plt.plot(sol.t,sol.y[0])
-plt.xlabel("Time (days)")
-plt.ylabel("Cell number")
-plt.show()
+class senescence_model:
+    def __init__(self,params):
+        """
+        Extract parameters for the senescence model, set state to none
+        """
+        self.eta = params[0]
+        self.beta = params[1]
+        self.kappa = params[2]
+        self.state = None
+    def initialisation(self,initial):
+        """ Initialisation of model"""
+        self.state = initial
+    
+    def senescent_model(self,t,x):
+        """Minimal model for cellular senescence:
+        dx/dt = eta*x - beta*x/(x-kappa)
+        
+        Parameters: 
+        :x: Number of senescent cells
+        :dx/dt: Rate of change of number of cells
+        :eta: Increase of senescent cell production with increased age
+        :beta: Rate of senescent cell removal
+        :kappa: half-way saturation point for removal (Michaelis-Menten)
+        """
+        dxdt = self.eta*x-(self.beta*x)/(x-self.kappa)
+        return dxdt
+    def solve(self,t):
+        """Solve using SciPy (deterministic here, no stochastic element)"""
+        sol = solve_ivp(self.senescent_model, [t[0],t[-1]],self.state,args=self.params)
+        return sol
