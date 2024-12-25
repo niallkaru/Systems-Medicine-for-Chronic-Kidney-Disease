d9e9e44ea1ef95daca48d4337209a59123e2e7dc
niallkaru <niallkarunaratne@gmail.com>
2024-12-25 21:45:29 +0000

    Added fibrosis model

--- fibrosis_model.py
+++ fibrosis_model.py
@@ -4,3 +4,36 @@ from scipy.integrate import solve_ivp
 Class for fibrosis model for cells (within kidney)
 Adapted from Principles of Cell Circuits for Tissue Repair and Fibrosis Adler et al. 2020"""
 
+class fibrosis_model:
+    def __init__(self,params,initial_state):
+        """"Extract several million parameters (is there a better way?)"""
+        self.stateM = initial_state[0]
+        self.stateF = initial_state[1]
+        self.stateP = initial_state[2]
+        self.stateC = initial_state[3]
+
+        self.lam1 = self.params[0]
+        self.mu1 = self.params[1]
+        self.lam2 = self.params[2]
+        self.mu2 = self.params[3]
+        self.beta1 = self.params[4]
+        self.beta2 = self.params[5]
+        self.beta3 = self.params[6]
+        self.alpha1 = self.params[7]
+        self.alpha2 = self.params[8]
+        self.gamma = self.params[8]
+        self.k1 = self.params[9]
+        self.k2 = self.params[10]
+        self.K = self.params[11]
+
+    def equations(self,M,F,C,P,lam1,lam2,mu1,mu2,beta1,beta2,beta3,alpha1,alpha2,gamma,k1,k2,K):
+        dFdt = F*(lam1*(P/(k1+P))*(1-(F/K))-mu1)
+        dMdt = M*(lam2*(C/(k2+C))-mu2)
+        dCdt = beta1*F - alpha1*M*(C/(k2+C))-gamma*C
+        dPdt = beta2*M+beta3*F-alpha2*F*(P/(k1+P))-gamma*P
+        return np.array([dFdt,dMdt,dCdt,dPdt])
+    
+    def solve(self,t):
+        
+        sol = solve_ivp(self.equations,[t(0),t[-1]],np.array(self.stateM,self.stateF,self.stateP,self.stateC),t_eval)
+
--- main.py
+++ main.py
@@ -15,8 +15,5 @@ def main():
     plt.show()
 
 
-
-
-
 if __name__ == '__main__':
     main()
